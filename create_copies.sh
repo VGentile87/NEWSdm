3816ccd28e5f529b532408f6ff1b2c44c86e20fd
VGentile87 <valgent1987@gmail.com>
2020-02-19 15:49:59 +0100

    Added new script with time integration of small clusters

--- create_copies.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-#
-# CRYSTAL 70nm sim with SRIM
-cp /media/vale/TOSHIBA\ EXT/SRIM_SIMULATION/crystals_70nm/SRIM_input/Make_SRIM.C Sim_NIT_crystals/NIT_70nm/SRIM_input/
-cp /media/vale/TOSHIBA\ EXT/SRIM_SIMULATION/crystals_70nm/SRIM_input/Make_SRIM.h Sim_NIT_crystals/NIT_70nm/SRIM_input/
